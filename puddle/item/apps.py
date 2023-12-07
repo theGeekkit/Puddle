60cff29ade731972c7d4498af2d5861f1fe89244
theGeekkit <sonofgondolin16@gmail.com>
2023-12-07 17:37:25 -0600

    12-7-23

--- puddle/item/apps.py
+++ puddle/item/apps.py
@@ -4,3 +4,9 @@ from django.apps import AppConfig
 class ItemConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'item'
+    
+    def ready(self):
+        from .models import Category
+        
+        from django.contrib import admin
+        admin.site.register(Category)
