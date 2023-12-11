0d199533e031dd5a6608168af140025d7813f712
theGeekkit <sonofgondolin16@gmail.com>
2023-12-11 09:22:28 -0600

    added item

--- puddle/item/models.py
+++ puddle/item/models.py
@@ -3,3 +3,17 @@ from django.db import models
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=255)
+    
+    class Meta:
+        ordering = ('name',)
+        verbose_name_plural = 'Categories'
+        
+    def __str__(self):
+        return self.name
+    
+class Item(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.Textfield(blank=True, null=True)
+    price = models.FloatField()
+    is_sold = models.BooleanField(default=False)
+    created_at = models.DateTimeField()
