c3b1c5c76b4480c62e690d11fa7f7ea23ad37308
theGeekkit <sonofgondolin16@gmail.com>
2023-12-11 17:44:06 -0600

    added pillow alongside item aspects

--- puddle/item/models.py
+++ puddle/item/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
@@ -12,8 +13,11 @@ class Category(models.Model):
         return self.name
     
 class Item(models.Model):
+    category = models.ForeignKey(Category, related_name='items', on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
     price = models.FloatField()
+    image = models.ImageField(upload_to='item_images', blank=True, null=True)
     is_sold = models.BooleanField(default=False)
-    created_at = models.DateTimeField()
+    created_by = models.ForeignKey(User, related_name='items', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
