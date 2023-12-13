dff343186249e213f9bf2849d7de3f40e44ea43f
theGeekkit <sonofgondolin16@gmail.com>
2023-12-13 10:09:00 -0600

    adding signup

--- puddle/puddle/urls.py
+++ puddle/puddle/urls.py
@@ -17,14 +17,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from core.views import index, contact
+
 from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    path('', index, name='index'),
-    path('items/', include('item.urls')),
-    path('contact/', contact, name='contact'),
+    path('', include('core.urls')),
+    path('items/', include('item.urls')),    
     path('admin/', admin.site.urls),
 ]
 if settings.DEBUG:
