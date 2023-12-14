34e1482063c8312ec9eafcb601c30fdd3185ef2e
theGeekkit <sonofgondolin16@gmail.com>
2023-12-14 16:44:25 -0600

    12-14-23 added log in auth

--- puddle/core/views.py
+++ puddle/core/views.py
@@ -6,7 +6,7 @@ from item.models import Category, Item
 from .forms import SignupForm
 
 def index(request):
-     items = Item.objects.filter(is_sold=False) [0:6]
+     items = Item.objects.filter(is_sold=False)[0:6]
      categories = Category.objects.all()
      return render(request, 'core/index.html', {
           'categories': categories,
@@ -17,7 +17,7 @@ def contact(request):
     return render(request, 'core/contact.html')
 
 def signup(request):
-     if request.method == 'POST':
+     if request.method == "POST":
           form = SignupForm(request.POST)
           
           if form.is_valid():
