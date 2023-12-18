0b869dd33f7528b21a352f44b17da453bbbe3089
theGeekkit <sonofgondolin16@gmail.com>
2023-12-18 10:28:00 -0600

    added dashboard and add item form

--- puddle/puddle/urls.py
+++ puddle/puddle/urls.py
@@ -23,9 +23,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('', include('core.urls')),
-    path('items/', include('item.urls')),    
+    path('items/', include('item.urls')),
+    path('dashboard/', include('dashboard.urls')),    
     path('admin/', admin.site.urls),
-]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
